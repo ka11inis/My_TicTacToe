048509c005f0186a6f7c580c95439f525a58e7b3
ka11inis <leka11inis@gmail.com>
2020-05-13 21:07:56 +0300

    Created HumanMoves && Check Empty Board

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXTABLE 25 
 #define USETABLE 9
 int board[ MAXTABLE ];
-enum pioni{E, O, X, B};
+enum {E, O, X, B};
+enum {HUMAN_WIN, COMPUTER_WIN, DRAW};
+
 const int convertTo25[USETABLE] = {
     6,7,8,
     11,12,13,
@@ -21,46 +25,77 @@ void initBoard(){
 }
 
 void printBoard(){
-    for (int i=0; i<MAXTABLE; i++){  
-        if (i%5 == 0  && i!=0)
-            printf("\n");
-        //board[i] = i;
-        //printf("\t%d",board[i] );
-        switch (board[i]){
-            case X:
-                printf("\tX");
-                break;
-            case E:
-                printf("\tE");
-                break;
-            case O:
-                printf("\tO");
-                break;
-            case B:
-                printf("\t+");
+    char intToChar[] = "-OX+";
+    printf("\nBoard\n");
+    for (int i=0; i<USETABLE; i++){  
+        if (i%3 == 0  && i!=0)  printf("\n");
+        printf("%4c",intToChar[ board[convertTo25[i]] ] );
         }
-    }
     printf("\n");
 }
 
-void runGame(){
-   int place = -1;
+int hasEmpty(){
+    for (int i=0; i<USETABLE; i++){
+        if (board[convertTo25[i]] == E) 
+            return 1;
+    }
+    return 0;
+}
+
+void makeMove(int sq, int side){
+    board[sq] = side;
+}
+
+
+int getHumanMove(){
+
+    int place = -1;
     do{  
-        printf("Επιλεξτε θεση για το  Χ apo to 1-9");
-        if ( scanf("%d",&place) == 1 && (place >=0 || place <=9)){
-            //getchar();
+        printf("Επιλεξτε θεση για το  Χ apo to 0-8");
+        if ( scanf("%d", &place) == 1 
+            && (place >=0 && place <=8) 
+            && board[convertTo25[place]] == E){ 
+            getchar();
             printf("edoses %d\n",place);    
-            board[ place ] = X;
-            printBoard();
+            //board[ convertTo25[place] ] = X;
+            //printBoard();
         }else{
-            printf("mono arithmous apo 0-9 kai edoses  %d \n ", place);
+            printf("mono arithmous apo 0-8 kai edoses  %d \n ", place);
             place = getchar();
         }
         fflush(stdin);
     }while( place<0 || place>9);
+return convertTo25[place];
+}
+void runGame(){
+    int gameOver = 0,
+    side = X,
+    lastMoveMade = 0;
+    
+
+    initBoard();
+
+    while(!gameOver){
+        if (side == X){
+            lastMoveMade  = getHumanMove();
+            makeMove( lastMoveMade, side);
+            side = O;
+            printBoard();
+        }else{
+        }
+
+
+        if(!hasEmpty()){
+            printf("DRAW\n");
+            gameOver = 1;            
+        }
+    
+    }
+
 }
 
 int main (int argc, char* argv[]){
+
     initBoard();
     printBoard();
     runGame();
