f3066457398e9b49c81a9f3e11bd47742ca2d437
ka11inis <leka11inis@gmail.com>
2020-05-22 19:35:55 +0300

    Added getDirection() and FindThreeInARow(), to checked if there is a Winner

--- TicTacToe.c
+++ TicTacToe.c
@@ -9,7 +9,7 @@
 //Αρχικοποίηση μεταβλητών και πινάκων
 //Initialization variable and tables
 int board[ MAXTABLE ];
-enum {E, O, X, B};
+enum {X, O, E, B};
 enum {HUMAN_WIN, COMPUTER_WIN, DRAW};
 
 //Χρησιμοποιείται για την μετατροπή του πίνακα με τα όρια σε 
@@ -26,6 +26,29 @@ const int convertTo25[USETABLE] = {
 //It is for the directions which are used to check the winner
 const int Directions[4] = {1, 5, 4, 6};
 
+int findThreeInARow(const int ourIndex, const int side){
+    int dirIndex = 0, dir = 0, threeCount = 1;
+    
+    for(dirIndex = 0; dirIndex < 4; dirIndex++){
+        dir = Directions[dirIndex];
+        threeCount += getDirection(ourIndex + dir, dir, side);
+        threeCount += getDirection(ourIndex + dir * -1, dir * -1, side);
+        if (threeCount == 3) break;
+        threeCount = 1;
+    }
+    return threeCount;
+}
+
+int getDirection(int startSq, const int dir, const int side){
+    int found = 0;
+    while(board[startSq] !=  B){
+        if(board[startSq] != side) break;
+        found++;
+        startSq += dir;
+    }
+    return found;
+}
+
 //Αρχικοποίηση πίνακα 
 //Initialization table
 void initBoard(){
@@ -40,7 +63,7 @@ void initBoard(){
 //Εκτύπωση Board
 //Print Board
 void printBoard(){
-    char intToChar[] = "-OX+";
+    char intToChar[] = "XO-+";
     printf("\nBoard\n");
     for (int i=0; i<USETABLE; i++){  
         if (i%3 == 0  && i!=0)  printf("\n");
@@ -129,9 +152,16 @@ void runGame(){
 
         //Έλεγχος για το εάν υπάρχει νικητής
         //Check if there is a winner
-//        if (){
-
-  //      }
+        if (findThreeInARow(lastMoveMade, side^1) == 3){
+            printBoard();
+            printf("Game Over!\n");
+            if(side == X){
+                printf("Computer ( O )  wins\n");
+            }else{
+                printf("Human ( X ) wins\n");
+            }
+            gameOver = 1;
+        }
 
         //Έλεγχος για το εάν υπάρχει άδεια θέση αλλιώς είναι ισοπαλία 
         //Check if there are any empty places otherwise it is a draw
@@ -141,7 +171,6 @@ void runGame(){
         }
     
     }
-
 }
 
 int main (int argc, char* argv[]){
